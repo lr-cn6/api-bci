52ad08fcd38122142b8b50dcaf467d66bd737e8e
Luis Riveros <luis.riveros_ex@scotiabank.cl>
2019-12-02 23:19:51 -0300

    feat(start): Initial project

---
+++ src/main/java/cl/globallogic/recruiting/apibci/ApiBciApplication.java
@@ -0,0 +1,13 @@
+package cl.globallogic.recruiting.apibci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/globallogic/recruiting/apibci/ApiBciApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.globallogic.recruiting.apibci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
